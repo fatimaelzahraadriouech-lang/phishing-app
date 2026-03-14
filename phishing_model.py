4fd98d42d5d14e37fceeb69b29523d9c9fcb2cba
fatimaelzahraadriouech-lang <fatimaelzahraadriouech@gmail.com>
2026-03-14 14:11:13 +0100

    Update phishing_model.py

--- phishing_model.py
+++ phishing_model.py
@@ -24,7 +24,8 @@ print("ÉTAPE 3 : Chargement du dataset")
 print("=" * 50)
 
 # ⚠️ Mets le bon nom de ton fichier CSV ici
-df = pd.read_csv(r"C:\Users\fatima driouech\Desktop\projet_phishing\archive\phishing.csv")
+df = pd.read_csv("phishing.csv")
+
 
 print(f"✅ Dataset chargé : {df.shape[0]} lignes, {df.shape[1]} colonnes")
 print("\nAperçu des données :")
